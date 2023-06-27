58ed3adc55a9710ff123226184a39a6f2d8e845b
Ebelechukwu Doris Okolo <dorisebele47@gmail.com>
2023-06-27 10:44:06 +0100

    folder structure created

---
+++ src/registeration/SIgnUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+const SignUp = () =>{
+    return(
+        <div className="container">
+           <div className="outer-div">
+                <div className="avater-part"></div>
+                <div></div>
+           </div>
+        </div>
+    );
+  
+}
+export default SignUp;
